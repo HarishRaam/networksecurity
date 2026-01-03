d66ddaa0eebfaece311819f692f4720a42c0f726
“HarishRaam” <getintouchwithharish@gmail.com>
2026-01-03 17:23:53 +0530

    Model Training

--- main.py
+++ main.py
@@ -2,7 +2,8 @@ import sys
 from networksecurity.components.data_ingestion import DataIngestion
 from networksecurity.components.data_validation import DataValidation
 from networksecurity.components.data_transformation import DataTransformation
-from networksecurity.entity.config_entity import DataIngestionConfig, DataValidationConfig, DataTransformationConfig
+from networksecurity.components.model_trainer import ModelTrainer
+from networksecurity.entity.config_entity import DataIngestionConfig, DataValidationConfig, DataTransformationConfig, ModelTrainerConfig
 from networksecurity.entity.config_entity import TrainingPipelineConfig
 from networksecurity.exception.exception import NetworkSecurityException
 from networksecurity.logging.logger import logging
@@ -31,6 +32,14 @@ if __name__ == "__main__":
         datatransformationartifact = datatransformation.initiate_data_transformation()
         print(datatransformationartifact)
         logging.info("Data transformation completed")
+        
+        logging.info('Model training started')
+        modeltrainerconfig = ModelTrainerConfig(training_pipleline_config=trainingpipelineconfig)
+        model_trainer = ModelTrainer(data_transformation_artifact=datatransformationartifact, 
+                     model_trainer_config=modeltrainerconfig)
+        modeltrainerartifact = model_trainer.initiate_model_training()
+        logging.info("Model training artifact created")
+        print(modeltrainerartifact)
         
     except Exception as e:
         raise NetworkSecurityException(e, sys)
--- networksecurity/utils/main_utils/utils.py
+++ networksecurity/utils/main_utils/utils.py
@@ -4,6 +4,8 @@ from networksecurity.logging.logger import logging
 import os, sys
 import numpy as np
 import pickle
+from sklearn.model_selection import GridSearchCV
+from sklearn.metrics import accuracy_score
 
 def read_yaml_file(file_path:str) -> dict:
     
@@ -44,10 +46,21 @@ def save_numpy_array_data(file_path:str, array:np.array):
     except Exception as e:
         raise NetworkSecurityException(e, sys) from e
     
+def load_numpy_array_data(file_path:str):
+    
+    try:
+        if not file_path:
+            raise Exception(f'File path {file_path} does not exist')
+        
+        with open(file_path, 'rb') as file_obj:
+            return np.load(file_path)
+    except Exception as e:
+        raise NetworkSecurityException(e, sys) from e
+    
 def save_object(file_path:str, obj:object):
     
     try:
-        logging.info("Entered the save object method of main utils class")
+        logging.info("Saving Object using save_object method of utils class")
         dir_name = os.path.dirname(file_path)
         os.makedirs(dir_name, exist_ok=True)
         with open(file_path, 'wb') as file_obj:
@@ -55,4 +68,49 @@ def save_object(file_path:str, obj:object):
         logging.info("Exited the save object method of main utils class")
     except Exception as e:
         raise NetworkSecurityException(e, sys) from e
+    
+def load_object(file_path:str) -> object:
+    
+    try:
+        logging.info("Reading the object using load_object method of utils class")
+        if not os.path.exists(file_path):
+            raise Exception(f'The file path {file_path} does not exist')
+        
+        with open(file_path, 'rb') as file_obj:
+            logging.info("Loading file object")
+            return pickle.load(file_obj)
+    except Exception as e:
+        raise NetworkSecurityException(e, sys) from e
+    
+def evaluate_models(x_train, y_train, x_test, y_test, models, params) -> tuple:
+    
+    try:
+        report = {}
+        parameters = {}
+        
+        for key, value in models.items():
+            model = value
+            model_param = params[key]
+            logging.info(f"Training model {key} with parameters {model_param}")
+            
+            gs = GridSearchCV(model, param_grid=model_param, cv=3, n_jobs=1)
+            gs.fit(x_train, y_train)
+            
+            model.set_params(**gs.best_params_)
+            model.fit(x_train, y_train)
+            
+            y_train_pred = model.predict(x_train)
+            y_test_pred = model.predict(x_test)
+            
+            #model_train_accuracy = accuracy_score(y_train, y_train_pred)
+            model_test_accuracy = accuracy_score(y_test, y_test_pred)
+            
+            report[key] = model_test_accuracy
+            parameters[key] = gs.best_params_
+            
+        return report, parameters
+                
+    except Exception as e:
+        raise NetworkSecurityException(e, sys)
+    
     
